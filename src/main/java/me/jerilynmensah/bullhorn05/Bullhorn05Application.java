20a9ae1888c845feb11dc4034e05cadf431c8ef1
jrmensah <jmensah4@student.umuc.edu>
2017-08-25 17:01:31 -0400

    Initial commit

---
+++ src/main/java/me/jerilynmensah/bullhorn05/Bullhorn05Application.java
@@ -0,0 +1,12 @@
+package me.jerilynmensah.bullhorn05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bullhorn05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bullhorn05Application.class, args);
+	}
+}
